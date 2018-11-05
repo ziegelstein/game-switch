0cc3ad7ca322cee8c3a9b91c33ebe48c6f3c48d9
Felix <Felix.Frings@yahoo.de>
2018-11-05 22:37:19 +0100

    Initial Commit

---
+++ scripts/Global.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# This is the Global Node, it is the Interface to all Game modules.
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
