1e91a3c1736646602f371b556181c4d9ae4ec62f
Philipp Meyer <phmeyer555@gmail.com>
2018-11-09 22:53:18 +0100

    Audio_Manager added

--- scripts/Audio_Manager.gd
+++ scripts/Audio_Manager.gd
@@ -3,30 +3,52 @@ extends Node
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var player
+var sounds = {}
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	player = AudioStreamPlayer.new()
+	
+	# Maybe register default sounds
+	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
-func loadFile(file):
+func registerSound(file, key):
+	var player = AudioStreamPlayer.new()
+	self.add_child(player)
 	player.stream = load(file)
-
-func playLoaded():
 	if(player.stream):
-		player.play()
+		sounds[key] = player
+	else:
+		logger.error("Soundfile "+file+" not found")
 
-func play(file):
-	player.stream = load(file)
-	player.play()
+func unregisterSound(key):
+	if(sounds.has(key)):
+		sounds[key].stop()
+		sounds[key].queue_free()
+		sounds.erase(key)
+	else:
+		logger.error("Couldn't unregister sound "+key+", not registered")
+
+func playSound(key):
+	if(sounds.has(key)):
+		var player = sounds[key]
+		player.play()
+	else:
+		logger.error("Couldn't play sound "+key+", not registered")
+
+func stopSound(key):
+	if(sounds.has(key)):
+		var player = sounds[key]
+		if(player.playing):
+			player.stop()
+	else:
+		logger.error("Couldn't stop sound "+key+", not registered")
 
 func debug():
-	loadFile("res://Media/soundstest/punk.wav")
-	playLoaded()
+	Audio_Manager.registerSound("res://Media/sounds/no_sh.wav", 'no_sh')
+#	Audio_Manager.playSound('no_sh')
+#	#Audio_Manager.unregisterSound('no_sh')
+#	#Audio_Manager.playSound('no_sh')
+#	#play("res://Media/sounds/no_sh.wav")
 	return "Audio_Manager done"
